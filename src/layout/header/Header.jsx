95571acc2077ee799e11c138a02d5cae34f7a4b9
lisaseayou <lisa.machy@hotmail.fr>
2022-01-18 16:12:09 +0100

    first commit

---
+++ src/layout/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <div>
+      <img
+        src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png"
+        alt="Wild Code School logo"
+      />
+      <p>LES ARGONAUTES</p>
+    </div>
+  );
+}
+
+export default Header;
